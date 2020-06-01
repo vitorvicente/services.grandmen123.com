e8df364b954e614d93007014187eaf22557d7d71
vitorvicente <vitor@bu.edu>
2020-06-01 11:08:49 +0100

    Initial Setup for ReactJS

---
+++ src/constants/Routes.js
@@ -0,0 +1,48 @@
+export const LANDING = "/";
+
+export const ABOUT = "/about";
+export const TOS = "/about/tos";
+export const VIP = "/about/vip";
+export const VOUCHES = "/about/vouches";
+
+export const SERVICES = "/services";
+
+export const MC = "/services/minecraft";
+export const MCORDER = "/services/minecraft/order";
+export const MCORDER2 = "/services/minecraft/order-other";
+
+export const MCOTHER = "/services/minecraft/other";
+export const MCOTHERDC = "/services/minecraft/other/donationcraft";
+export const MCOTHERE = "/services/minecraft/other/enjin";
+export const MCOTHERLTM = "/services/minecraft/other/long-term";
+export const MCOTHERNO = "/services/minecraft/other/network-organization";
+export const MCOTHERP = "/services/minecraft/other/panel-instalation";
+export const MCOTHERSM = "/services/minecraft/other/server-migration";
+export const MCOTHERSR = "/services/minecraft/other/server-reset";
+export const MCOTHERT = "/services/minecraft/other/tebex";
+export const MCOTHERX = "/services/minecraft/other/xenforo";
+
+export const MCPC = "/services/minecraft/plugin-configurations";
+
+export const MCSS = "/services/minecraft/server-setups";
+export const MCSSB = "/services/minecraft/server-setups/bungee";
+export const MCSSC = "/services/minecraft/server-setups/creative";
+export const MCSSF = "/services/minecraft/server-setups/factions";
+export const MCSSK = "/services/minecraft/server-setups/kitpvp";
+export const MCSSL = "/services/minecraft/server-setups/lobby";
+export const MCSSP = "/services/minecraft/server-setups/prison";
+export const MCSSSB = "/services/minecraft/server-setups/skyblock";
+export const MCSSS = "/services/minecraft/server-setups/survival";
+
+export const NMC = "/services/non-minecraft";
+
+export const NMCO = "/services/non-minecraft/order";
+export const NMCOO = "/services/non-minecraft/order-other";
+
+export const NMCA = "/services/non-minecraft/app-development";
+export const NMCC = "/services/non-minecraft/consultation";
+export const NMCD = "/services/non-minecraft/discord-development";
+export const NMCS = "/services/non-minecraft/software-development";
+export const NMCW = "/services/non-minecraft/web-development";
+
+export const CONTACT = "/contact";
